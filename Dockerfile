e13b63b83a0f9afb7d6ffd0faba029ff850f1962
Andreas Iwerbo <andreas.iwerbo@geshdo.com>
2020-10-22 08:16:55 +0200

    first commit

---
+++ Dockerfile
@@ -0,0 +1,18 @@
+## Build Environment
+FROM node:12-slim AS build-env
+WORKDIR /app/react-app
+COPY ./react-app/package.json .
+COPY ./react-app .
+RUN npm install
+RUN ["npm", "run-script", "build"]
+WORKDIR /app/gatsby-app
+COPY ./gatsby-app/package.json .
+COPY ./gatsby-app .
+RUN npm install
+RUN ["npm", "run", "build"]
+RUN rm -rf public/react-app
+WORKDIR /app/react-app
+RUN mv build ../gatsby-app/public/react-app
+FROM nginx 
+EXPOSE 80
+COPY --from=build-env /app/gatsby-app/public /usr/share/nginx/html
---
+++ react-app/src/App.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import logo from './logo.svg';
+import { BrowserRouter, Route, Link } from 'react-router-dom'
+import './App.css';
+
+function App() {
+  return (
+    <BrowserRouter>
+    <Route
+    path="/react-app"
+    render={() => (
+      <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+        <Link to="/react-app-page2">Go to second react page</Link>
+      </header>
+    </div>
+    )}
+     />
+    <Route
+    path="/react-app-page2"
+    render={() => (
+      <div>
+        <div>
+          <Link to="/react-app">Go to react page 1</Link>
+        </div>
+        <div>
+          <a href="/">Back to gatsby</a>
+        </div>
+      </div>
+    )}
+    />
+    </BrowserRouter>
+  );
+}
+
+export default App;
